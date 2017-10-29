3976573c59d80ce38d5c8260c79da1bd84ef7d6e
汪中杰 <jay_wzj@163.com>
2017-10-29 13:18:52 +0800

    修改测试

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { appRouters } from './app.routers';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot(),
+    RouterModule.forRoot(appRouters)
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routers.ts
@@ -0,0 +1,16 @@
+import { RouterModule } from '@angular/router';
+//首页
+import { HomeComponent } from './home/home.component';
+
+export const appRouters = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+    // loadChildren:'./home/home.module#HomeModule'
+  }
+]
